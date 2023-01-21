f7f0cbbf1be33f19eec56955b13682c27c0ac9dc
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-21 14:29:15 +0300

    Example 01 Tablica Square

---
+++ Ex_01_TablSquare/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести таблицу квадратов от 1 до N
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+for(int i = 1; i <= N; i++)
+    {
+        int result = i*i;
+        Console.WriteLine(result);
+    }
