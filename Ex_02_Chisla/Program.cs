2c1b25dda72947c4a43f80f27e7e0b128bb52122
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-21 14:50:13 +0300

    Example 02 Chisla

---
+++ Ex_02_Chisla/Program.cs
@@ -0,0 +1,11 @@
+﻿// Сумму 
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int number = int.Parse(Console.ReadLine());
+int result = 0;
+for(int i = 1; i <= number; i++)
+    {
+        result = result+i;
+        Console.WriteLine(result);
+    }
