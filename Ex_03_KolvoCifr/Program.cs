938df34694ec043f8865244e40a82c93667f0fcc
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-21 15:08:53 +0300

    Example 03 Kolichestvo cifr

---
+++ Ex_03_KolvoCifr/Program.cs
@@ -0,0 +1,16 @@
+﻿// Определить количество цифр в числе
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+while (num)
+
+
+
+// int result = 0;
+// for(int i = 1; i <= number; i++)
+//     {
+//         result = result+i;
+//         Console.WriteLine(result);
+//     }
